629d23930cfe517153153b11e5a83eac571f2c2c
ISESCEM <office.ise@sahyadri.edu.in>
2025-09-01 06:58:03 +0530

    program1.py

---
+++ program1.py
@@ -0,0 +1,29 @@
+# name = input("Enter your name: ")
+# print (f"Hello, {name}!")
+
+
+# import sys
+# for line in sys.stdin:
+#     print("You entered", line.strip())
+
+
+# import sys
+# sys.stdout.write("Enter your name");
+# name=sys.stdin.readline().strip()
+# sys.stdout.write(f"Hello, {name}!\n")
+
+
+# name = "###############**********Aman*************######################"
+# rename=name.strip('#')
+# print("Before Stripping:", name)
+# print("After Stripping",rename)
+
+# import sys
+# name = sys.stdin.readline()
+# print("Your name",name)
+
+
+file1=open("samplefile.txt","w")
+file1.write("Are you okay?")
+print()
+file1.close()
